f1a01e95af1f14ef90d352bce06fbff162ce8e06
Alee <aleeherasimiuk@gmail.com>
2021-01-06 17:27:53 -0300

    Deleted comments

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
 
@@ -16,19 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ADFRadio',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
@@ -55,8 +42,6 @@ class HomePage extends StatelessWidget{
 }
 
 class Button extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -66,9 +51,6 @@ class Button extends StatelessWidget {
           final snackBar = SnackBar(
             content: Text('Yay! A SnackBar!'),
           );
-
-          // Find the Scaffold in the widget tree and use
-          // it to show a SnackBar.
           Scaffold.of(context).showSnackBar(snackBar);
         },
         child: Text('Show SnackBar'),
