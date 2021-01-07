d371b6ce5c57afe360cb62d6a1627a2b77c48aca
Alee <aleeherasimiuk@gmail.com>
2021-01-07 00:01:21 -0300

    extracted some features from button

--- lib/ui/buttons.dart
+++ lib/ui/buttons.dart
@@ -1,17 +1,44 @@
 import 'package:adfradio/radio_api/radio_api.dart';
 import 'package:flutter/material.dart';
 
-class StatefulButton extends StatefulWidget {
+class StatelessButton extends StatelessWidget {
 
   final radius;
+  final icon;
   final onPressed;
   final iconColor = Colors.white;
   final buttonColor = Color.fromRGBO(39, 39, 39, 0.4);
   final alignment = Alignment.center;
   final iconSize = 40.0;
 
+  StatelessButton({Key key, this.radius, this.onPressed, this.icon});
 
-  StatefulButton({Key key, this.radius, this.onPressed});
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Container(
+          width: radius,
+          height: radius,
+          alignment: alignment,
+          decoration: boxDecoration(),
+          child: Icon(icon, size: iconSize, color: iconColor)
+      ),
+      style: circleButtonStyle(buttonColor),
+      onPressed: onPressed,
+    );
+  }
+}
+
+
+class StatefulButton extends StatefulWidget {
+
+  final radius;
+  final iconColor = Colors.white;
+  final buttonColor = Color.fromRGBO(39, 39, 39, 0.4);
+  final alignment = Alignment.center;
+  final iconSize = 40.0;
+  
+  StatefulButton({Key key, this.radius});
 
   @override
   _StatefulButtonState createState() => _StatefulButtonState();
@@ -33,25 +60,29 @@ class _StatefulButtonState extends State<StatefulButton> {
 
   @override
   Widget build(BuildContext context) {
-
     return ElevatedButton(
       child: Container(
           width: widget.radius,
           height: widget.radius,
           alignment: widget.alignment,
-          decoration: BoxDecoration(shape: BoxShape.circle),
+          decoration: boxDecoration(),
           child: Icon(icon, size: widget.iconSize, color: widget.iconColor)
       ),
-      style: ElevatedButton.styleFrom(
-          shape: CircleBorder(),
-          primary: widget.buttonColor
-      ),
+      style: circleButtonStyle(widget.buttonColor),
       onPressed: (){
+        RadioApi.toggleState();
         setState(() {
           setIcon();
         });
-        RadioApi.toggleState();
       },
     );
   }
+}
+
+BoxDecoration boxDecoration() => const BoxDecoration(shape: BoxShape.circle);
+ButtonStyle circleButtonStyle(buttonColor) {
+  return ElevatedButton.styleFrom(
+      shape: const CircleBorder(),
+      primary: buttonColor
+  );
 }
