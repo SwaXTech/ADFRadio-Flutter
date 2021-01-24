5c21cfd850054ac9e7324a6c74da90fd9243f9ee
Alee <aleeherasimiuk@gmail.com>
2021-01-24 13:18:04 -0300

    Added fade in/out with getx

--- lib/screens/radio.dart
+++ lib/screens/radio.dart
@@ -23,43 +23,53 @@ class RadioScreen extends StatelessWidget{
               children: [
                 TopTitle(),
                 Expanded(
-                  child: Stack(
-                    children: [
-                      Center(
-                        child: Container(
-                            padding: EdgeInsets.only(left: 20.0, top: 8.0),
-                            child: Image(image: AssetImage("assets/dots.png"),)
-                        ),
-                      ),
-                      Center(
-                        child: Container(
-                            padding: EdgeInsets.only(left: 20.0, top: 8.0),
-                            child: Image(image: AssetImage("assets/opaque_wave.png"),)
-                        ),
-                      ),
-                      Center(
-                        child: GetBuilder<WaveController>(
-                          init: waveController,
-                          builder: (_controller) => Obx(() => AnimatedOpacity(
-                              duration: Duration(seconds: 2),
+                  child: GetBuilder<WaveController>(
+                    init: waveController,
+                    builder: (_controller) => Stack(
+                      children: [
+                        Center(
+                          child: Obx(() => AnimatedOpacity(
+                              duration: Duration(milliseconds: 1500),
                               opacity: _controller.opacity,
                               child: Container(
                                   padding: EdgeInsets.only(left: 20.0, top: 8.0),
-                                  child: Image(image: AssetImage("assets/gradient_wave.png")),
+                                  child: Image(image: AssetImage("assets/dots.png"),)
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      Center(
-                        child: Container(
-                          child: SongInfo(),
-                          height: 270,
-                          width: 270,
-                          decoration: BoxDecoration(shape: BoxShape.circle, color: Color.fromRGBO(45, 47, 60, 1)),
+                        Center(
+                          child: Obx(() => AnimatedOpacity(
+                              duration: Duration(seconds: 1),
+                              opacity: _controller.opacity,
+                              child: Container(
+                                  padding: EdgeInsets.only(left: 20.0, top: 8.0),
+                                  child: Image(image: AssetImage("assets/opaque_wave.png"),)
+                              ),
+                            ),
+                          ),
                         ),
-                      )
-                    ],
+                        Center(
+                          child: Obx(() => AnimatedOpacity(
+                                duration: Duration(milliseconds: 500),
+                                opacity: _controller.opacity,
+                                child: Container(
+                                    padding: EdgeInsets.only(left: 20.0, top: 8.0),
+                                    child: Image(image: AssetImage("assets/gradient_wave.png")),
+                                ),
+                              ),
+                            ),
+                          ),
+                        Center(
+                          child: Container(
+                            child: SongInfo(),
+                            height: 270,
+                            width: 270,
+                            decoration: BoxDecoration(shape: BoxShape.circle, color: Color.fromRGBO(45, 47, 60, 1)),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 PlayerButtons(waveController: waveController),
